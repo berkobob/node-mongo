bc1268c2860dff994cae26d5d380f5571df8e8ab
Berko Bob <alever@omniis.com>
2019-01-22 13:55:52 +0000

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-    "mongodb://localhost:27017/TodoApp",
+    process.send.MONGODB_URI || "mongodb://localhost:27017/TodoApp",
     {
         useMongoClient: true,
     },
