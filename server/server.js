f654e72ea630da8497edfa5ec3d5de60ee27d09f
BerkoBob <alever@omniis.com>
2019-01-23 20:21:06 +0000

    Complete model

--- server/server.js
+++ server/server.js
@@ -101,6 +101,19 @@ app.patch("/todos/:id", (req, res) => {
     });
 });
 
+app.post("/users", (req, res) => {
+  var user = new User(_.pick(req.body, ["email", "password"]));
+
+  user
+    .save()
+    .then(doc => {
+      res.send(doc);
+    })
+    .catch(e => {
+      res.status(400).send(e);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
